c13ea922f65c083a4f8dce2beb7d7bcbc91f89cc
dao <gary.vo32@gmail.com>
2022-03-20 21:27:05 -0700

    able to extract info for new book object. need to find a way to store local

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let bookList = [];
 
+// JSON.stringify(localStorage.setItem('bookList', '[]'));
+
 function Book(author, title, numOfPages, readStatus) {
   this.author = author;
   this.title = title;
@@ -11,10 +13,26 @@ function showForm() {
   modal.style.display = 'block';
 }
 
+function submit() {
+  // let bookArr = JSON.parse(localStorage.getItem('bookList'));
+  // console.log(bookArr, 'test1')
+  let author = document.getElementById('author').value;
+  let title = document.getElementById('title').value;
+  let numOfPages = document.getElementById('pages').value;
+  let readStatus = document.getElementById('read-status').value;
+  let newBook = new Book(author,title,numOfPages, readStatus);
+  console.table(newBook);
+  bookList.push(newBook);
+  // console.log(bookArr, 'test2')
+  // JSON.stringify(localStorage.setItem('bookList', bookArr));
+}
+
 // open form within modal for user to add data
 let addButton = document.querySelector('.add-book');
 addButton.addEventListener('click', showForm);
 
+let formSubmit = document.querySelector('.form-submit');
+formSubmit.addEventListener('click', submit);
 
 // used to display book(s)
 let newBook = document.createElement('li');
