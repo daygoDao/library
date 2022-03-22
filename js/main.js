158b61bb024706c7b7b7af4cfbe1a576aa55e0f0
dao <gary.vo32@gmail.com>
2022-03-21 21:59:31 -0700

    got the checkbox of read status to output correctly

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function submit() {
   let author = document.getElementById('author').value;
   let title = document.getElementById('title').value;
   let numOfPages = document.getElementById('pages').value;
-  let readStatus = document.getElementById('read-status').value;
+  let readStatus = document.getElementById('read-status').checked;
   let newBook = new Book(author, title, numOfPages, readStatus);
 
   // add to book list
@@ -56,7 +56,6 @@ function showList() {
 
   let booklist = JSON.parse(localStorage.getItem('bookList'));
   for (let book of booklist) {
-    let read = (book.readStatus == 'on') ? true : false;
 
     // used to display book(s)
     let bookUL = document.querySelector('ul')
@@ -66,6 +65,7 @@ function showList() {
     let bookPages = document.createElement('p');
     let bookRead = document.createElement('p');
     let deleteBook = document.createElement('button');
+    deleteBook.classList.add('delete-book');
 
     let bookCard = document.createElement('section');
     bookCard.classList.add('card');
@@ -75,7 +75,7 @@ function showList() {
     bookAuthor.textContent = book.author;
     bookTitle.textContent = book.title;
     bookPages.textContent = book.numOfPages;
-    bookRead.textContent = read;
+    bookRead.textContent = read.readStatus;
     deleteBook.textContent = 'DELETE';
   
     console.log(book.numOfPages)
@@ -90,3 +90,13 @@ function showList() {
   }
 }
 
+// delete book
+let removeBooks = document.querySelectorAll('.delete-book');
+for (let book of removeBooks) {
+  book.addEventListener('click', discard);
+}
+
+function discard(e) {
+  console.log(e);
+  //e.currentTarget.parentNode.parentNode.remove(this);
+}
