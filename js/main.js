39710808b1b4382de06a45f3dd0e33393423341f
dao <gary.vo32@gmail.com>
2022-03-21 21:08:32 -0700

    fixed broken add book button. have the book card displayed to ui

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 showList();
 
+/////////////////////////////////////////////////////////////////////
+// open form within modal for user to add data
+let addButton = document.querySelector('.add-book');
+addButton.addEventListener('click', showForm);
+
+let formSubmit = document.querySelector('.form-submit');
+formSubmit.addEventListener('click', submit);
+
+// open modal on click
+let modal = document.querySelector('.modal');
+//kick out of modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+///////////////////////////////////////////////////////////////////
+
 function Book(author, title, numOfPages, readStatus) {
   this.author = author;
   this.title = title;
-  this.numofPages = numOfPages;
+  this.numOfPages = numOfPages;
   this.readStatus = readStatus;
 }
 
@@ -12,10 +30,6 @@ function showForm() {
 }
 
 function submit() {
-  // create if bookList doesnt exist
-  if (!localStorage.getItem('bookList')) {
-    localStorage.setItem('bookList', JSON.stringify([]));
-  }
 
   let bookArr = JSON.parse(localStorage.getItem('bookList'));
   let author = document.getElementById('author').value;
@@ -29,38 +43,50 @@ function submit() {
   // store to local
   localStorage.setItem('bookList', JSON.stringify(bookArr));
 
-  // output to ui
+  // reload page to update list
   window.location.reload();
-}
-
-// open form within modal for user to add data
-let addButton = document.querySelector('.add-book');
-addButton.addEventListener('click', showForm);
-
-let formSubmit = document.querySelector('.form-submit');
-formSubmit.addEventListener('click', submit);
-
-
-// open modal on click
-let modal = document.querySelector('.modal');
-
-
-//kick out of modal
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
 }
 
 // output local stored book list to ui
 function showList() {
+  // create if bookList doesnt exist
+  if (!localStorage.getItem('bookList')) {
+    localStorage.setItem('bookList', JSON.stringify([]));
+  }
+
   let booklist = JSON.parse(localStorage.getItem('bookList'));
   for (let book of booklist) {
+    let read = (book.readStatus == 'on') ? true : false;
+
     // used to display book(s)
     let bookUL = document.querySelector('ul')
     let bookLI = document.createElement('li');
-    console.log(book)
-    bookLI.textContent = book.author;
+    let bookAuthor = document.createElement('h3');
+    let bookTitle = document.createElement('h4');
+    let bookPages = document.createElement('p');
+    let bookRead = document.createElement('p');
+    let deleteBook = document.createElement('button');
+
+    let bookCard = document.createElement('section');
+    bookCard.classList.add('card');
+
+    console.log(read)
+
+    bookAuthor.textContent = book.author;
+    bookTitle.textContent = book.title;
+    bookPages.textContent = book.numOfPages;
+    bookRead.textContent = read;
+    deleteBook.textContent = 'DELETE';
+  
+    console.log(book.numOfPages)
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    bookCard.appendChild(deleteBook);
+
+    bookLI.appendChild(bookCard);
     bookUL.appendChild(bookLI);
   }
 }
+
